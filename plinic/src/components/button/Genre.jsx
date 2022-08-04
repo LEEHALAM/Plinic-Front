fed510f24883aa3c4ed17a5d9eab3ae883db0759
chaeeunj <official2930@naver.com>
2022-08-05 04:01:22 +0900

    :construction: (chaeeun/feature/components/#3) 장르 버튼 구현하는 중

--- plinic/src/components/button/Genre.jsx
+++ plinic/src/components/button/Genre.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../../styles/theme';
 
 function Genre() {
-  return <div>Genre</div>;
+  const [buttonActive, setButtonActive] = useState(false);
+
+  const onClickGenreButton = () => {
+    // if (buttonActive == true) {
+    //   setButtonActive(GenreButton);
+    // } else {
+    //   setButtonActive(GenreButtonClicked);
+    // }
+    // console.log(GenreButton);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GenreButton onClick={onClickGenreButton}>acoustic</GenreButton>
+    </ThemeProvider>
+  );
 }
 
 export default Genre;
+
+const GenreButton = styled.button`
+  width: 65px;
+  height: 30px;
+  border: solid 2px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.color.white};
+  ${({ theme }) => theme.align.flexCenter};
+  color: ${({ theme }) => theme.color.navy};
+  ${({ theme }) => theme.font.weight.thick};
+  ${({ theme }) => theme.font.size[10]};
+`;
+
+const GenreButtonClicked = styled.button`
+  width: 65px;
+  height: 30px;
+  border: solid 2px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.color.navy};
+  ${({ theme }) => theme.align.flexCenter};
+  color: ${({ theme }) => theme.color.white};
+  ${({ theme }) => theme.font.weight.thick};
+  ${({ theme }) => theme.font.size[10]};
+`;
