57fd88bc13bc6c8b9753cec00b106e44f199b27e
chaeeunj <official2930@naver.com>
2022-08-13 19:07:22 +0900

    :sparkles: (chaeeun/feature/components/#3) 장르 버튼 컴포넌트 생성

--- plinic/src/components/button/Genre.jsx
+++ plinic/src/components/button/Genre.jsx
@@ -1,48 +1,84 @@
-import React, { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import theme from '../../styles/theme';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { genreFirstLine, genreSecondLine, genreThirdLine, genreFourthLine } from './genreData';
 
 function Genre() {
-  const [buttonActive, setButtonActive] = useState(false);
+  const [isClicked, setIsClicked] = useState([]);
 
-  const onClickGenreButton = () => {
-    // if (buttonActive == true) {
-    //   setButtonActive(GenreButton);
-    // } else {
-    //   setButtonActive(GenreButtonClicked);
-    // }
-    // console.log(GenreButton);
+  const onClickGenreBtn = e => {
+    isClicked.includes(e.target.id)
+      ? setIsClicked(isClicked.filter(el => el !== e.target.id))
+      : isClicked.length < 3 && setIsClicked(prev => [...prev, e.target.id]);
   };
 
+  useEffect(() => {
+    console.log(isClicked);
+  }, [isClicked]);
+
   return (
-    <ThemeProvider theme={theme}>
-      <GenreButton onClick={onClickGenreButton}>acoustic</GenreButton>
-    </ThemeProvider>
+    <>
+      <Container>
+        {genreFirstLine.map(genre => (
+          <GenreButton key={genre.id} onClick={onClickGenreBtn} isClicked={isClicked} id={genre.name}>
+            {genre.name}
+          </GenreButton>
+        ))}
+      </Container>
+      <Container>
+        {genreSecondLine.map(genre => (
+          <GenreButton key={genre.id} onClick={onClickGenreBtn} isClicked={isClicked} id={genre.name}>
+            {genre.name}
+          </GenreButton>
+        ))}
+      </Container>
+      <Container>
+        {genreThirdLine.map(genre => (
+          <GenreButton key={genre.id} onClick={onClickGenreBtn} isClicked={isClicked} id={genre.name}>
+            {genre.name}
+          </GenreButton>
+        ))}
+      </Container>
+      <Container>
+        {genreFourthLine.map(genre => (
+          <GenreButton key={genre.id} onClick={onClickGenreBtn} isClicked={isClicked} id={genre.name}>
+            {genre.name}
+          </GenreButton>
+        ))}
+      </Container>
+    </>
   );
 }
 
 export default Genre;
 
-const GenreButton = styled.button`
-  width: 65px;
-  height: 30px;
-  border: solid 2px;
-  border-radius: 10px;
-  background-color: ${({ theme }) => theme.color.white};
-  ${({ theme }) => theme.align.flexCenter};
-  color: ${({ theme }) => theme.color.navy};
-  ${({ theme }) => theme.font.weight.thick};
-  ${({ theme }) => theme.font.size[10]};
+const NAVY = ({ theme }) => theme.color.navy;
+const WHITE = ({ theme }) => theme.color.white;
+
+const Container = styled.div`
+  width: 100vw;
+  height: fit-content;
+  ${({ theme }) => theme.align.flexCenter}
+  &:not(:last-of-type) {
+    margin-bottom: 5px;
+  }
+  &:nth-child(2n + 1) {
+    margin-left: 25px;
+  }
 `;
 
-const GenreButtonClicked = styled.button`
-  width: 65px;
+const GenreButton = styled.button`
+  width: 70px;
   height: 30px;
-  border: solid 2px;
-  border-radius: 10px;
-  background-color: ${({ theme }) => theme.color.navy};
+  border: solid 2px ${NAVY};
+  border-radius: 15px;
+  background-color: ${({ isClicked, id }) => (isClicked.includes(id) ? NAVY : WHITE)};
   ${({ theme }) => theme.align.flexCenter};
-  color: ${({ theme }) => theme.color.white};
+  color: ${({ isClicked, id }) => (isClicked.includes(id) ? WHITE : NAVY)};
   ${({ theme }) => theme.font.weight.thick};
   ${({ theme }) => theme.font.size[10]};
+  cursor: pointer;
+
+  &:not(:last-of-type) {
+    margin-right: 5px;
+  }
 `;
