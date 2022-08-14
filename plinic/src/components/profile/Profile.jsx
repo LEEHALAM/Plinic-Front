e642b3fbbef401609c17839232c1561e804675b1
Lami <Halam.Lee.Lami@gmail.com>
2022-08-15 03:50:39 +0900

    :construction: (halam/feature/components/#3) 프로필 구현 작업 중 - 버튼 제외 구현

---
+++ plinic/src/components/profile/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+
+function Profile({ width, img }) {
+  console.log(img);
+
+  return (
+    <Wrapper width={width} img={img}>
+      {img === undefined ? <ProfileImg icon={faCircleUser} width={width} /> : ''}
+    </Wrapper>
+  );
+}
+
+export default Profile;
+
+const Wrapper = styled.div`
+  width: ${({ width }) => (width ? width : 107)}px;
+  height: ${({ width }) => (width ? width : 107)}px;
+  border-radius: 50%;
+  background: url(${props => props.img}) no-repeat center/cover;
+`;
+
+const ProfileImg = styled(FontAwesomeIcon)`
+  font-size: ${({ width }) => (width ? width : 107)}px;
+`;
--- plinic/src/pages/test/HL.jsx
+++ plinic/src/pages/test/HL.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Profile from '../../components/profile/Profile';
 
 function HL() {
-  return <div>HL</div>;
+  return (
+    <>
+      <Profile width={100} img={'https://i.pinimg.com/736x/81/3d/39/813d3997687c5b5d13a5e3421df8101b.jpg'} />
+      <Profile width={140} />
+      <Profile width={150} />
+      <Profile width={200} img={'https://i.pinimg.com/originals/a1/ec/c2/a1ecc289bd9af7eec59f6f6ad43c1c14.jpg'} />
+    </>
+  );
 }
 
 export default HL;
