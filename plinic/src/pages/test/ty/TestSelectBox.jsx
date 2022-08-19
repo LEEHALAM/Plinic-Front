40481e7c109dc5ac7c929e2280115509caa6a8c1
taeyoung <rlaxodud911@gmail.com>
2022-08-20 02:59:26 +0900

    :memo: (taeyoung/feature/components/#3) 셀렉트 박스 컴포넌트 테스트 페이지 생성

---
+++ plinic/src/pages/test/ty/TestSelectBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SelectBox from '../../../components/select/SelectBox';
+
+function TestSelect() {
+  return (
+    <div>
+      <SelectBox sortBy={'playlists'} />
+      <SelectBox sortBy={'likes'} />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias molestiae autem omnis. Culpa magnam porro quidem
+        est corrupti quo aliquid iure deleniti dolores maiores, assumenda alias expedita animi, velit ipsa!
+      </div>
+    </div>
+  );
+}
+
+export default TestSelect;
